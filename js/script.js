1e22776b2051f305e9434bd833a4371e76626974
Cristiano <cristiano.tarconi@gmail.com>
2021-01-14 15:56:17 +0100

    timeout + user prompt js logic

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@
 Dopo 30 secondi l'utente deve inserire un prompt alla volta i numeri che ha visto precedentemente. 
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
 
+var seconds = 5;
 var minNum = 1;
 var maxNum = 50;
 var numList = [];
+var userList = [];
 
 var i = 0;
 do {
@@ -14,10 +16,28 @@ do {
         numList.push(rndmNumber);
     }
     i++;
-    
+
 } while (numList.length < 5);
 
 console.log(numList);
+alert(numList);
+
+setTimeout(() => {
+    var j = 0;
+    do {
+
+        var userNum = parseInt(prompt('Inserisci un numero'));
+
+        if (userList.includes(userNum) == false) {
+            userList.push(userNum);
+        }
+        j++;
+    } while (userList.length < 5);
+    console.log(userList);
+
+}, seconds * 1000);
+
+
 
 // - Funzioni -
 function generaRandom(min, max) {
